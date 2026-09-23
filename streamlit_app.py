f42dadbea4c0c9c5a51f5341d9e0e48d56b514eb
Paul3181 <33693698+Paul3181@users.noreply.github.com>
2026-09-23 17:18:55 +0000

    comm

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,5 +61,5 @@ if ingredients_list :
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
         
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
